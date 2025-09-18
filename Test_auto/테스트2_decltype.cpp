ce573af7496cadec29df026d2cc89dd9ce7ae39a
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-09-18 15:34:37 +0900

    [feat] update auto decltype test

---
+++ Test_auto/테스트2_decltype.cpp
@@ -0,0 +1,110 @@
+﻿// 각 변수의 타입은 무엇입니까?
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int a = 3;
+	decltype(a) b = 2;				//(	const int	) 
+
+	const int& ra = a;
+	decltype(ra) rb = b;			//(	const int&	) 
+
+	int&& x = 3;
+	decltype(x) y = 2;				//(	int&&	) 
+
+	struct A { double* d; };
+	A* aa;
+	decltype(aa->d) dd = 0.1;		//(	double*	) 
+
+	int c = 3;
+	decltype(c) c1 = 2;				//타입은 int
+	decltype((c)) c2 = c;			//타입은 int&
+
+	int x, y;
+	decltype(x + y) z;				//타입은?	int
+
+	int x1, y1;
+	decltype(x1 = y1) z1;			//타입은?	int&
+
+	int x2;
+	decltype(std::move(x2)) z2;		//타입은?	int&&
+
+}
+
+//---------------------------------------------------------------------
+//다음 선언에 대하여 형식은 ?
+
+#include <utility>
+
+const int&& fx();
+struct A { double x; };
+
+int main()
+{
+	const A* a = new A();
+
+	decltype(fx);			// 형식은 ???	const int&&()
+	decltype(fx());			// 형식은 ???	const int&&
+	decltype(a->x);			// 형식은 ???	double
+	decltype((a->x));		// 형식은 ???	double&
+}
+
+
+
+//---------------------------------------------------------------------
+//다음 선언에 대하여 형식은 ?
+
+#include <vector>
+
+struct S
+{
+	int m_x;
+	S() { m_x = 42; }
+};
+
+const S foo() { return S(); }                       // const 구조체 S() 를 리턴한다.
+const int& foobar() { return 123; }                 // const int & 를 리턴한다.
+
+int main() {
+
+	std::vector<int> vec = { 10, 20 };
+
+	auto a = foo();									//(	S	) 
+	typedef decltype(foo()) foo_type;				//(	const S	) 
+
+	auto b = foobar();								//(	const int&	) -> int
+	typedef decltype(foobar()) foobar_type;			//(	cosnt int&	) 
+
+	auto itr = vec.begin();							//(	iterator	) 
+	typedef decltype(vec.begin()) itr_type;			//(	iterator	) 
+
+	auto			 v1 = vec[0];					//(	int&	) 
+	decltype(vec[0]) v2 = vec[0];					//(	int	) 
+}
+
+//---------------------------------------------------------------------
+//맴버함수의 반환형을 리턴하는 함수 template을 작성하려고 합니다.
+//_______에 적당한 반환형을 선언을 하세요.
+
+#include <utility>
+
+struct A { int f() { return 10; }  };
+struct B { char f() { return 'A'; } };
+
+template <typename T>
+auto call_function(T& t) { return t.f(); }
+
+
+int main() {
+	A a;
+	auto val1 = call_function(a);	// int
+	B b;
+	auto val2 = call_function(b);	// char
+
+}
+
+
+
+
---
+++ Test_auto/테스트4_ranged for.cpp
@@ -0,0 +1,70 @@
+﻿
+//---------------------------------------------------------------------------------
+// array 배열을 전달해서 배열의 합을 구하려고 합니다. 
+// error의 이유는? 어떻게 수정하면 될까요?
+
+// 템플릿 인자로 사이즈를 넣어주면 됩니다.
+
+#include <iostream>
+#include <cstddef>
+
+template<std::size_t n>
+int sumArray( int (&arr)[n] )
+{
+	int sum = 0;
+	for (const auto& number : arr)		//error
+		sum += number;
+	return sum;
+}
+
+int main()
+{
+	int arr[5] = { 9, 7, 5, 3, 1 };
+	std::cout << sumArray(arr);
+	return 0;
+}
+
+
+//---------------------------------------------------------------------------------
+//범위기반 for 문을 사용해서 배열의 요소를 1씩 더하는 코드를 작성 하세요.
+#include <iostream>
+int main()
+{
+	int buff[10] = { 1,2,3,4,5 };
+
+	//code
+	for (auto& e : buff) e++;
+
+	for (auto e : buff) std::cout << e << " "; std::cout << "\n";
+
+
+	return 0;
+}
+//for (auto& e : buff) e++;
+
+
+//---------------------------------------------------------------------------------
+// B 클래스인지 검사해서 맞다면 맴버 값을 출력하는 코드를 작성하세요.
+#include <iostream>
+
+struct A { virtual void vf() {} };
+struct B : public A { int m = 100; };
+
+void memberPrint(A* a)
+{
+	if (B* b = dynamic_cast<B*>(a))
+	{
+		std::cout << b->m << "\n";
+	}
+	else
+	{
+		std::cout << "Not B class\n";
+	}
+}
+
+int main() {
+	A a;
+	B b;
+	memberPrint(&a);
+	memberPrint(&b);
+}
